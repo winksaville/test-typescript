94abffd3fa0fe32ec374beb060339003bcda5800
Wink Saville <wink@saville.com>
2017-02-03 12:44:41 -0800

    Add class Student

--- greeter.d.ts
+++ greeter.d.ts
@@ -1,2 +1,7 @@
-declare function greeter(person: string): string;
-declare var user: string;
+declare class Student {
+    private fullName;
+    constructor(firstName: string, middleName: string, lastName: string, suffix?: string);
+    greeter(): string;
+}
+declare let user0: Student;
+declare let user1: Student;
--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,19 @@
-function greeter(person: string) {
-  return "Hello, " + person;
+class Student {
+  private fullName: string;
+
+  constructor(firstName: string,
+              middleName: string,
+              lastName: string,
+              suffix: string = "") {
+    this.fullName = firstName + " " + middleName + " " + lastName + " " + suffix;
+  }
+
+  greeter(): string {
+      return "Hello, " + this.fullName;
+  }
 }
 
-var user = "Wink Saville";
+let user0 = new Student("Winthrop", "Lyon", "Saville",  "III");
+let user1 = new Student("Wink", "", "Saville");
 
-document.body.innerHTML = greeter(user);
+document.body.innerHTML = user0.greeter() + " aka: " + user1.greeter();
